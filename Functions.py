9c1e662cbaa5c0a48e1cb051a2ed2ba02ab18db6
Horacio Granados <granadoshoracio2@gmail.com>
2023-02-20 21:23:58 -0600

    Simple Calculator

--- Functions.py
+++ Functions.py
@@ -1,9 +1,34 @@
-def area(largo,ancho) : 
-    area = largo * ancho
-    return largo, ancho, area
+# Multiples returns
+def sumar(numero1,numero2) :
+    return numero1 + numero2, numero1, numero2
+
+def restar(numero1,numero2) :
+    return numero1 - numero2, numero1, numero2
+
+def multiplicar(numero1,numero2) :
+    return numero1 * numero2, numero1, numero2
+
+def dividir(numero1,numero2) :
+    return numero1 / numero2, numero1, numero2 
 
 
-largo, ancho, area_total = area(5,10)
-print("Largo : ", largo)
-print("Ancho : ", ancho)
-print("Area : ", area_total)
+print('\n------------Calculadora----------\n')
+print('1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir')
+opcion = int(input('Digite una opcion: '))
+
+if opcion == 1 : 
+    resultado,num1,num2 = sumar(5,5)
+    print(f'{num1} + {num2} = {resultado}')
+    
+if opcion == 2 : 
+    resultado,num1,num2 = restar(5,5)
+    print(f'{num1} - {num2} = {resultado}')
+    
+if opcion == 3 : 
+    resultado,num1,num2 = multiplicar(5,5)
+    print(f'{num1} * {num2} = {resultado}')
+    
+if opcion == 4 : 
+    resultado,num1,num2 = dividir(5,5)
+    print(f'{num1} / {num2} = {resultado}')
+
