38cc2d2cca2a0e7d0c1fbd188d7b6a1a0606c15f
Horacio Granados <granadoshoracio2@gmail.com>
2023-02-20 20:58:04 -0600

    Fist commit

---
+++ Functions.py
@@ -0,0 +1,9 @@
+def area(largo,ancho) : 
+    area = largo * ancho
+    return largo, ancho, area
+
+
+largo, ancho, area_total = area(5,10)
+print("Largo : ", largo)
+print("Ancho : ", ancho)
+print("Area : ", area_total)
